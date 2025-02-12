8688285099c3fcd4dad9669213353c771eab19ed
psc <psc@psc.de>
2025-02-12 11:16:12 +0100

    Initialize variables, set structure of the code.

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-#include
-#include
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "parser.h"
 
-int main(){
+int main(int argc, char* argv[]){
+    FILE* asm_file;
+    int current_line; // unnecessary?
+    long internal_fileptr;
+    int buffer_size = 250;
+    char* buffer[buffer_size];
+    char* reduced_buffer[3];
+    FILE* hack_file;
+//-- Initialize files and variables --//
+    // open the file with the assembly code
+    argv++;
+    strcat(argv, ".txt");
+    asm_file = fopen(argv,"r");
 
+    // open the file for the generated binary code
+
+//---------- Parse ---------------//
+
+//---------- Translate ----------//
+
+    fclose(asm_file);
 }
