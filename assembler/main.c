ee273b9a23404c0343e97a10d17410ea866908b1
mattpasca <mattpascalis@gmail.com>
2025-02-26 12:52:38 +0100

    Update main.c
    Change parser function names.
    Initialize hack file

--- assembler/main.c
+++ assembler/main.c
@@ -1,34 +1,47 @@
-// TODO buffer has to be without whitespace
+/* TODO pass arguments for the binary functions
+ *      as &binary[3] for comp
+ *         &binary[11] for dest
+ *         &binary[14] for jump
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include "parser.h"
+#include "code.h"
 
 FILE* asm_file;
+FILE* hack_file;
+char hack_file_name[20];
 bool more_commands;
 char type;
 char dest_mnemonic[3];
 char comp_mnemonic[10];
 char jump_mnemonic[3];
 char current_symbol[20];
+char binary[16];
 char buffer[250];
-FILE* hack_file;
 
 int main(int argc, char* argv[]){
 
-//-- Initialize files and variables --//
+//-- Initialize files --//
     if(argc==0){
          printf("Please provide Assembly Code!");
          exit(EXIT_FAILURE);
     }
-    more_commands = true;
-
-    // open the file for the generated binary code
     initializer(argv[1]);
 
-//---------- Parse ---------------//
+    // Open the .hack file for the generated binary code
+    strcpy(hack_file_name, argv[1]);
+    char* substring_ptr;
+    substring_ptr = strstr(hack_file_name, &point);
+    memset(substring_ptr, '\0', strlen(substring_ptr));
+    strcat(hack_file_name, ".hack")
+    hack_file = fopen(hack_file_name, "w");
+
+//---------- Parse and Translate---------------//
+    more_commands = true;
     while(more_commands==true){
         advance();
         type = commandType();
@@ -37,18 +50,17 @@ int main(int argc, char* argv[]){
             continue;
         }
         if(type=='C'){
-            dest();
-            comp();
-            jump();
+            dest_parser();
+            comp_parser();
+            jump_parser();
+
         }else{
             symbol();
         }
+
         more_commands = hasMoreCommands();
     }
 
-
-//---------- Translate ----------//
-
     fclose(asm_file);
- // fclose(hack_file);
+    fclose(hack_file);
 }
